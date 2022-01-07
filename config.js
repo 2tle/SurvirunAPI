e5d21a69a9a865033881b136462bfdfb1d043b5a
Hyunju Yang <iam@2tle.io>
2022-01-07 21:56:48 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "secret" : "SoMeThingSeCrET41!@#%",
-  "mongodb": "mongodb+srv://sunrinton:hackerthon@sunrin.uu2d0.mongodb.net/sunrin",
+  "secret" : "",
+  "mongodb": "",
   "port": 3000,
-  "hostname":"dicon21.2tle.io"
+  "hostname":""
 }
